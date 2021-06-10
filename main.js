c8e6690fd7d2d22db5085a70d1318ff2c021a721
EddyBautista <bautista.j.eddy@gmail.com>
2021-06-09 20:07:14 -0500

    env var

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const Discord = require("discord.js");
+const config = require('dotenv').config()
+
 const client = new Discord.Client();
 
+// check to see what bot i am logged under 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
 
+// Whenever a user types in test in chat it will respond with whatever is in msg.reply -- need to connect to a quote api 
 client.on("message", msg => {
-    if(msg.content === "ping") {
-        msg.reply("Pong");
+    if(msg.content.toLocaleLowerCase() === "test") {
+        msg.reply("App is fixed");
     }
 })
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN) // pulled from the env file 
