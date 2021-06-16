60113b5d193608605b61a54a3aab6a371bfbd335
EddyBautista <bautista.j.eddy@gmail.com>
2021-06-16 13:45:52 -0500

    connected to zen api

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 const Discord = require("discord.js");
+const fetch = require("node-fetch")
 const config = require('dotenv').config()
 
 const client = new Discord.Client();
 
+
+// call to the zen api for quotes and return the quote from json 
+const getQuote = () => {
+    return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+        return res.json() // sends a json response 
+    })
+    .then(data => {
+        return data[0]["q"] + " -" + data[0]["a"] // grab the quote and author and format it for the discord mssg
+    })
+}
+
 // check to see what bot i am logged under 
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.tag}!`)
+    console.log(`I am bot :  ${client.user.tag}!`)
 })
 
 // Whenever a user types in test in chat it will respond with whatever is in msg.reply -- need to connect to a quote api 
 client.on("message", msg => {
-    if(msg.content.toLocaleLowerCase() === "test") {
-        msg.reply("App is fixed");
-    }
+   if (msg.author.bot) return
+   if (msg.content.toLocaleLowerCase() === "$inspire") {
+       getQuote().then(quote => msg.channel.send(quote))
+   }
 })
 
 client.login(process.env.TOKEN) // pulled from the env file 
